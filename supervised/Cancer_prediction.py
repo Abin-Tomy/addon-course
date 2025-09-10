6d89dab70389af55522f90c3895dc314a65ea260
Abin-Tomy <abintomy163@gmail.com>
2025-09-10 16:26:09 +0530

    new-commit

--- supervised/Cancer_prediction.py
+++ supervised/Cancer_prediction.py
@@ -1,1 +1,28 @@
 import pandas as pd
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+data = load_breast_cancer()
+df = pd.DataFrame(data.data, columns=data.feature_names)
+df['target'] = data.target
+
+df_small = df[['mean radius', 'mean texture', 'mean perimeter', 'mean area', 'mean smoothness', 'target']]
+
+x= df_small.drop('target', axis=1)
+y= df_small['target']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42,stratify=y)
+
+
+
+
+model = LogisticRegression() 
+model.fit(x_train, y_train)
+
+
+y_pred = model.predict(x_test)
+y_proba = model.predict_proba(x_test)[:, 1]
